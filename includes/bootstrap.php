7d96850af686f5f919e1864f89e96f654e7d8bfd
Ross McKay <rmckay@webaware.com.au>
2019-04-17 14:54:13 +1000

    remove extraneous cruft in bootstrap loader
    it's been copypasta'd from another plugin

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -6,9 +6,8 @@ if (!defined('ABSPATH')) {
 
 /**
 * kick start the plugin
-* needs to hook at priority 0 to beat Event Espresso's load_espresso_addons()
 */
 add_action('plugins_loaded', function() {
 	require NGG_DLGALL_PLUGIN_ROOT . 'includes/class.NextGENDownloadGallery.php';
 	NextGENDownloadGallery::getInstance()->pluginStart();
-}, 0);
+});
